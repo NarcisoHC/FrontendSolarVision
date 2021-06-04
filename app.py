8281bccb557c52846410af3282354d46ee780e8c
Narciso Courtois <narhercour1@gmail.com>
2021-06-04 10:17:49 +0200

    added image resizer

--- app.py
+++ app.py
@@ -3,15 +3,46 @@ import requests
 import os
 #import uuid
 from google.cloud import storage
+#from PIL import Image
+
 
 st.set_option('deprecation.showfileUploaderEncoding', False)
 
 st.title('SolarVision V1.0')
 
-
 st.markdown('''
 Please, upload an image with a rooftop and we will classify it depending if it has or not a solar panel.
 ''')
+
+def adjust_image(image):
+    if image.size[0]==image.size[1]:
+        if image.size[0]!=320:
+            image=im.resize((320,320))
+        else:
+            image=image
+    else:
+        if image.size[0]>image.size[1]:        
+            width = int(round(image.size[0]*(320/image.size[1])))
+            image=image.resize((width,320))
+            left = (image.size[0]-image.size[1])/2
+            top = 0
+            right = (image.size[0]-image.size[1])/2 +image.size[1]
+            bottom = image.size[1]
+            image = image.crop((left, top, right, bottom))
+        
+        else:
+            image=image.rotate(90, expand=True)
+            width = int(round(image.size[0]*(320/image.size[1])))
+            image=image.resize((width,320))
+            left = (image.size[0]-image.size[1])/2
+            top = 0
+            right = (image.size[0]-image.size[1])/2 +image.size[1]
+            bottom = image.size[1]
+            image = image.crop((left, top, right, bottom))
+            image=image.rotate(270)
+    
+    return image 
+
 
 uploaded_file = st.file_uploader(label='upload .jpg or .png file', 
                                  type=['jpg', 'png'])
@@ -24,6 +55,7 @@ if uploaded_file is not None:
             uploaded_file.name = 'test_file.jpg' 
         if uploaded_file.name[-3:] == 'png':
             uploaded_file.name = 'test_file.png'
+        uploaded_file = adjust_image(uploaded_file)
         with open(os.path.join('tempDir', uploaded_file.name), 'wb') as f:
             f.write(uploaded_file.getbuffer())
         storage_client = storage.Client()
